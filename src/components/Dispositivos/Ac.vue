1672d1c1da3f307a9f42f692bca800f7418e242b
Ignacio Sagues <isagues@itba.edu.ar>
2020-05-16 21:25:43 -0300

    Anda con mas cosas

--- src/components/Dispositivos/Ac.vue
+++ src/components/Dispositivos/Ac.vue
@@ -15,21 +15,29 @@
                     <v-container class="py-0"> <!--class="px-3 py-0" -->
                         <v-row align="center" dense justify="space-around"><!--class="my-0 py-0" -->
                             <v-col>
-                                <v-switch dense></v-switch><!--class="px-3 my-auto" -->
+                                <v-switch v-model="status.value" dense></v-switch><!--class="px-3 my-auto" -->
                             </v-col>
                             <v-col>
                                 <v-btn icon >
-                                    <v-icon>mdi-minus</v-icon>
+                                    <v-icon @click="decreaseTemperature()">mdi-minus</v-icon>
                                 </v-btn>
                             </v-col>
                             <v-col>
-                                <v-btn text small>24ºC</v-btn>
+                                <v-text-field
+                                        class="inputNumber"
+                                        v-model="props.state.temperature"
+                                        solo rounded flat outlined dense
+                                        suffix="º"
+                                        type="number"
+                                        hide-details="true"
+                                ></v-text-field>
                             </v-col>
                             <v-col>
                                 <v-btn icon >
-                                    <v-icon>mdi-plus</v-icon>
+                                    <v-icon @click="increaseTemperature()">mdi-plus</v-icon>
                                 </v-btn>
                             </v-col>
+
                             <v-col ><!--class="pr-10" -->
                                 <v-btn text @click="controllerHandler()">{{extraControllers.message}}</v-btn>
                             </v-col>
@@ -100,13 +108,10 @@
                                 </v-list-item>
                             </v-col>
                             <v-col md="10" class="py-0"> <!--class="pr-10" -->
-                                <v-btn-toggle rounded dense>
-                                    <v-btn text>Auto</v-btn>
-                                    <v-btn text>-90</v-btn>
-                                    <v-btn text>-45</v-btn>
-                                    <v-btn text>0</v-btn>
-                                    <v-btn text>45</v-btn>
-                                    <v-btn text>90</v-btn>
+                                <v-btn-toggle v-model="props.state.horizontalSwing" rounded dense>
+                                    <v-btn v-for="mode in swing.horizontal.supportedValues" text :key="mode" :value="mode">
+                                        {{mode}}
+                                    </v-btn>
                                 </v-btn-toggle>
                             </v-col>
                         </v-row>
@@ -136,12 +141,8 @@
                                 </v-list-item>
                             </v-col>
                             <v-col md="10" class="py-0"> <!--class="pr-10" -->
-                                <v-btn-toggle rounded dense>
-                                    <v-btn text>Auto</v-btn>
-                                    <v-btn text>25</v-btn>
-                                    <v-btn text>50</v-btn>
-                                    <v-btn text>75</v-btn>
-                                    <v-btn text>100</v-btn>
+                                <v-btn-toggle v-model="props.state.fanSpeed" rounded dense>
+                                    <v-btn v-for="mode in fan.supportedValues" text :key="mode" :value="mode">{{mode}}</v-btn>
                                 </v-btn-toggle>
                             </v-col>
                         </v-row>
@@ -190,6 +191,18 @@
                         supportedValues: null,
                         action: 'setHorizontalSwing',
                     }
+                },
+                fan:{
+                    supportedValues: null,
+                    action: 'setFanSpeed',
+                },
+                temperature: {
+                    minValue:null,
+                    maxValue:null,
+                    action:'setTemperature',
+                },
+                status:{
+                    value: this.props.state.status === 'on',
                 }
             }
         },
@@ -223,13 +236,23 @@
                 this.swing.horizontal.supportedValues = params[0].supportedValues;
                 console.log(this.swing.horizontal.supportedValues);
             },
+            loadSupportedFanSpeeds(params){
+                this.fan.supportedValues = params[0].supportedValues;
+                console.log(this.fan.supportedValues);
+            },
+            loadSupportedTemperature(params){
+                this.temperature.minValue = params[0].minValue;
+                this.temperature.maxValue = params[0].maxValue;
+                console.log(this.temperature.minValue);
+                console.log(this.temperature.maxValue);
+            },
             increaseTemperature() {
-                if (this.temperature.value < this.temperature.maxValue)
-                    this.temperature.value += 5;
+                if (this.props.state.temperature < this.temperature.maxValue)
+                    this.props.state.temperature += 1;
             },
             decreaseTemperature() {
-                if (this.temperature.minValue < this.temperature.value)
-                    this.temperature.value -= 5;
+                if (this.temperature.minValue < this.props.state.temperature)
+                    this.props.state.temperature -= 1;
             },
             controllerHandler() {
                 this.extraControllers.value = !this.extraControllers.value;
@@ -249,6 +272,12 @@
             lib.deviceTypeActionParams(this.props.type.id, this.swing.horizontal.action)
                 .then(this.loadSupportedHorizontalSwing)
                 .catch( errors => console.log(`Horizontal swing - Supported values  ${errors}`) );
+            lib.deviceTypeActionParams(this.props.type.id, this.fan.action)
+                .then(this.loadSupportedFanSpeeds)
+                .catch( errors => console.log(`Fan speed - Supported values  ${errors}`) );
+            lib.deviceTypeActionParams(this.props.type.id, this.temperature.action)
+                .then(this.loadSupportedTemperature)
+                .catch( errors => console.log(`Temperature - Supported values  ${errors}`) );
 
         },
         watch:{
@@ -256,7 +285,47 @@
                 this.props.execute(this.mode.action, [this.props.state.mode])
                     .then(console.log)
                     .catch( errors => console.log(`Mode - Update value ${errors}`) );
+            },
+            'props.state.verticalSwing'(){
+                this.props.execute(this.swing.vertical.action, [this.props.state.verticalSwing])
+                    .then(console.log)
+                    .catch( errors => console.log(`Vertical swing - Update value ${errors}`) );
+            },
+            'props.state.horizontalSwing'(){
+                this.props.execute(this.swing.horizontal.action, [this.props.state.horizontalSwing])
+                    .then(console.log)
+                    .catch( errors => console.log(`Vertical swing - Update value ${errors}`) );
+            },
+            'props.state.fanSpeed'(){
+                this.props.execute(this.fan.action, [this.props.state.fanSpeed])
+                    .then(console.log)
+                    .catch( errors => console.log(`Fan speed - Update value ${errors}`) );
+            },
+            'props.state.temperature'(){
+
+                //TODO hacer el debounce del input
+                this.props.execute(this.temperature.action, [this.props.state.temperature])
+                    .then(console.log)
+                    .catch( errors => console.log(`Temperature - Update value ${errors}`) );
+            },
+            'status.value'(){
+                if(this.status.value){
+                    this.props.state.status = 'on';
+                    this.props.execute('turnOn')
+                        .then(console.log)
+                        .catch( errors => console.log(`Status - Update value ${errors}`) );
+                }
+                else{
+                    this.props.state.status = 'off';
+                    this.props.execute('turnOff')
+                        .then(console.log)
+                        .catch( errors => console.log(`Status - Update value ${errors}`) );
+                }
+                console.log(this.props.state.status);
             }
+            // 'props.state.status'(){
+            //
+            // }
         }
     }
 </script>
