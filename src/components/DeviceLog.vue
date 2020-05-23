db462532345eb525fdf97c04de7056be445d548d
Ignacio Sagues <isagues@itba.edu.ar>
2020-05-23 12:33:46 -0300

    Device logs con tabla

--- src/components/DeviceLog.vue
+++ src/components/DeviceLog.vue
@@ -1,87 +1,87 @@
 <template>
-    <v-container>
-        <v-row v-if="!loadingPage">
-            <v-col>
+    <div>
+        <nav-bar></nav-bar>
+        <v-dialog v-model="loadingPage" hide-overlay persistent width="30%">
+            <v-card color="primary" dark>
                 <v-card-text>
-                    <div>{{`${device.getRoomName()} - ${device.getHomeName()}`}}</div>
-                    <p class="display-1 text--primary">
-                        {{device.getName()}}
-                    </p>
+                    Cargando historial de acciones. Por favor espere :D
+                    <v-progress-linear indeterminate color="white" class="mb-0"
+                    ></v-progress-linear>
                 </v-card-text>
-                <p v-show="noLogs">
-                    Todavia no has realizado ninguna accion con este dispositivo!!
-                </p>
-                <v-card-text v-for="log in logs" :key="log.timestamp" v-show="!noLogs">
-                    <p>{{log.timestamp}}</p>
-                    <p>{{log.result}}</p>
-                    <p>{{log.action}}</p>
-                    <p>Params:</p>
-                    <p v-for="(param, i) in log.params" :key="i">{{param}}</p>
-                </v-card-text>
-            </v-col>
-            <v-col v-show="!noLogs">
-                <v-pagination
-                        v-model="currentPage"
-                        :disabled="awaitingLogs"
-                        :length="lastPage"
-                        :next-icon="nextIcon"
-                        :prev-icon="prevIcon"
-                        :page="currentPage"
-                        :total-visible="totalVisible"
-                        @input="getNewPage"
-                ></v-pagination>
-            </v-col>
-        </v-row>
-    </v-container>
+            </v-card>
+        </v-dialog>
+        <v-container >
+            <v-row v-if="!loadingPage" justify="center" :id="`#${device.id}`">
+                <v-col cols="8">
+                    <v-card>
+                        <v-container>
+                            <v-row>
+                                <v-col cols="12" class="pl-8">
+                                    <disp-info :device="device"></disp-info>
+                                </v-col>
+                                <v-col cols="12" class="align-baseline">
+                                    <v-container>
+                                        <v-row align="baseline">
+                                            <v-col order="start">
+                                                <v-card-title>
+                                                    Historial de acciones
+                                                </v-card-title>
+                                            </v-col>
+                                            <v-col order="end">
+                                                <v-text-field
+                                                        v-model="search"
+                                                        append-icon="mdi-magnify"
+                                                        label="Search"
+                                                        single-line
+                                                        hide-details
+                                                ></v-text-field>
+                                            </v-col>
+                                        </v-row>
+                                    </v-container>
+                                </v-col>
+                                <v-col cols="12">
+                                    <v-data-table
+                                            :headers="headers"
+                                            :items="logs"
+                                            :search="search"
+                                            no-data-text="No hay acciones para mostrar"
+                                    ></v-data-table>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+
 </template>
 
 <script>
     import Api from "../assets/js/Api";
     import Device from "../assets/js/Device";
+    import NavBar from "./NavBar";
+    import DispInfo from "./Dispositivos/DispInfo";
     //let moment = require('moment');
 
     export default {
         name: "deviceLog",
+        components: {DispInfo, NavBar},
         data(){
             return{
-                pageSize: 5,
-                totalVisible: 8,
-                nextIcon: 'l',
-                prevIcon: 'r',
                 device: null,
-                currentPage: 1,
-                lastPage: 1,
-                endReached: false,
                 logs: null,
-                awaitingLogs: false,
                 loadingPage: true,
-                noLogs: false
-            }
-        },
-        methods: {
-            checkLastPage(){
-                if(!this.endReached) {
-                    this.device.getLogs(this.pageSize, this.lastPage * this.pageSize)
-                        .then(data => {
-                            if(!(data.result.filter( log => log.result).length === 0))
-                                this.lastPage++;
-                            else
-                                this.endReached = true;
-                        })
-                        .catch(console.log);
-                }
-
-            },
-            getNewPage(){
-                this.awaitingLogs = true;
-                this.device.getLogs(this.pageSize, (this.currentPage - 1) * this.pageSize)
-                    .then( data => {
-                            this.logs = data.result.filter( log => log.result);
-                            if(this.currentPage === this.lastPage)
-                                this.checkLastPage();
-                    })
-                    .catch(console.log)
-                    .finally( () => this.awaitingLogs = false);
+                search: null,
+                headers: [
+                    {
+                        text: 'Fecha',
+                        align: 'start',
+                        value: 'timestamp',
+                    },
+                    { text: 'Accion', value: 'action' },
+                    { text: 'Parametros', value: 'params' },
+                ],
             }
         },
         mounted() {
@@ -93,17 +93,13 @@
                     this.device = new Device(data.result.id, data.result.name, data.result.type, data.result.meta, data.result.state, data.result.room);
 
                     this.awaitingLogs = true;
-                    this.device.getLogs(this.pageSize, 0)
+                    this.device.getLogs()
                         .then( data => {
                             this.logs = data.result.filter( log => log.result);
-                            if(this.logs.length === 0)
-                                this.noLogs = true;
-                            else
-                                this.checkLastPage();
+                            this.logs.forEach(log => log.timestamp = new Date(log.timestamp).toLocaleString())
                         })
                         .catch(console.log)
                         .finally( () => {
-                            this.awaitingLogs = false;
                             this.loadingPage = false;
                         });
                 })
@@ -119,3 +115,34 @@
 <style scoped>
 
 </style>
+
+<!--                <v-col>-->
+<!--                    <v-card-text>-->
+<!--                        <div>{{`${device.getRoomName()} - ${device.getHomeName()}`}}</div>-->
+<!--                        <p class="display-1 text&#45;&#45;primary">-->
+<!--                            {{device.getName()}}-->
+<!--                        </p>-->
+<!--                    </v-card-text>-->
+<!--                    <p v-show="noLogs">-->
+<!--                        Todavia no has realizado ninguna accion con este dispositivo!!-->
+<!--                    </p>-->
+<!--                    <v-card-text v-for="log in logs" :key="log.timestamp" v-show="!noLogs">-->
+<!--                        <p>{{log.timestamp}}</p>-->
+<!--                        <p>{{log.result}}</p>-->
+<!--                        <p>{{log.action}}</p>-->
+<!--                        <p>Params:</p>-->
+<!--                        <p v-for="(param, i) in log.params" :key="i">{{param}}</p>-->
+<!--                    </v-card-text>-->
+<!--                </v-col>-->
+<!--                <v-col v-show="!noLogs">-->
+<!--                    <v-pagination-->
+<!--                            v-model="currentPage"-->
+<!--                            :disabled="awaitingLogs"-->
+<!--                            :length="lastPage"-->
+<!--                            :next-icon="nextIcon"-->
+<!--                            :prev-icon="prevIcon"-->
+<!--                            :page="currentPage"-->
+<!--                            :total-visible="totalVisible"-->
+<!--                            @input="getNewPage"-->
+<!--                    ></v-pagination>-->
+<!--                </v-col>-->
