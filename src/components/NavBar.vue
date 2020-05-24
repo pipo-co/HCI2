09e84f8e774894e97e412a1efb46f2b465420019
Ignacio Vazquez <igvazquez@itba.edu.ar>
2020-05-24 18:21:12 -0300

    404 en español, cards menos redondeadas

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -36,13 +36,15 @@
                 </v-text-field>
                 </v-form>
             </v-col>
-            <v-col class="px-0 justify-end" order="end">
+            <v-col cols="1">
+                <v-btn icon @click="openSearchBar()">
+                    <v-icon>mdi-magnify</v-icon>
+                </v-btn>
+            </v-col>
+            <v-col class="px-0 justify-end" cols="3" order="end">
                 <v-tabs right color="#3C3F58" background-color="#72E1C7">
                     <v-tabs-slider color="#87FFE3"></v-tabs-slider>
-                    <v-tab @click="openSearchBar()">
-                        <v-icon right>mdi-magnify</v-icon>
-                    </v-tab>
-                    <v-tab class="blue-grey--text text--darken-4 font-weight-bold" to="/favoritos" >Favoritos</v-tab>
+                    <v-tab class="blue-grey--text text--darken-4 font-weight-bold ml-0" to="/favoritos" >Favoritos</v-tab>
                     <v-tab class="blue-grey--text text--darken-4 font-weight-bold" to="/">Hogares</v-tab>
                     <v-tab class="blue-grey--text text--darken-4 font-weight-bold" to="/rutinas">Rutinas</v-tab>
                 </v-tabs>
